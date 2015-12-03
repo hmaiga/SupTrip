075ee025ba27ab74c302ef8d1f988e48a7082722
gilda <gilda@Z>
2015-12-03 16:09:37 +0100

    modif

--- WebContent/Views/Home/Index.jsp
+++ WebContent/Views/Home/Index.jsp
@@ -1,12 +1,10 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ page language="java" contentType="text/html; charset=UTF8" pageEncoding="UTF8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
-</body>
-</html>
+<c:import url="/Views/Shared/Header.jsp"/>
+
+
+<p>ceci est ma page d'acceuil</p>
+
+
+<c:import url="/Views/Shared/Footer.jsp"/>
--- src/com/suptrip/servlets/HomeServlet.java
+++ src/com/suptrip/servlets/HomeServlet.java
@@ -27,7 +27,7 @@ public class HomeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("Views/Home/index.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/Views/Home/Index.jsp");
 		rd.forward(request, response);
 	}
 
@@ -35,7 +35,7 @@ public class HomeServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
 	}
 
 }
